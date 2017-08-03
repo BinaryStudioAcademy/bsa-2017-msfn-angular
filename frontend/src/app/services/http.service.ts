57a75e74cc5c43f27a654b5fc2c37b2e21217979
Haosik <noobbaster@gmail.com>
2017-08-04 02:46:26 +0300

    init commit

--- frontend/src/app/services/http.service.ts
+++ frontend/src/app/services/http.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class HttpService {
 
   constructor() { }
+
+  httpHandler(json) {}
 
 }
