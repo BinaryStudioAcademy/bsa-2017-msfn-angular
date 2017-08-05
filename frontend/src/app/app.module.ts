6d625e40a14ff42dea2c074c09f1823a1abac8d6
Stas <chstas2@gmail.com>
2017-08-05 15:36:48 +0300

    add md, validation, fix style

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -39,7 +39,9 @@ import {
     MdCheckboxModule,
     MdButtonModule,
     MdDialogModule,
-    MdSlideToggleModule
+    MdSlideToggleModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+
+  EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  email: string;
+  password: string;
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern(this.EMAIL_REGEX)
+  ]);
+
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ]);
 
   constructor() { }
 
-  ngOnInit() {
+  login() {
   }
-
 }
