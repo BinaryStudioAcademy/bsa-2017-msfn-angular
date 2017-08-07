bedb7c4a22570b9b16832a4b8ac48472744442c2
Dmitriy Beseda <besedadg@gmail.com>
2017-08-07 09:45:42 +0300

    webpack warning fix

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss', '../globalStyles/materialtheme.scss'],
+  styleUrls: ['./app.component.scss', '../globalStyles/materialTheme.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
