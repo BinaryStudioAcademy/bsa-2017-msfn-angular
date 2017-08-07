f32856dd40bef360b59c5b48bcec2e1bb6d5ef09
Dmitriy Beseda <besedadg@gmail.com>
2017-08-07 11:00:09 +0300

    syntax fixes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
-import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
-import { LoginComponent } from './components/login/login.component';
-import { RestorePasswordComponent } from './components/restore-password/restore-password.component';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { TestHttpComponent } from './components/test-http/test-http.component';
-import { HeaderViewComponent } from './components/header-view/header-view.component';
-import { RegistrationComponent } from './components/registration/registration.component';
+import {ForgetPasswordComponent} from './components/forget-password/forget-password.component';
+import {RestorePasswordComponent} from './components/restore-password/restore-password.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {TestHttpComponent} from './components/test-http/test-http.component';
+import {HeaderViewComponent} from './components/header-view/header-view.component';
+import {RegistrationComponent} from './components/registration/registration.component';
 import {LoginComponent} from './components/login/login.component';
 // import { ForLoggedInGuard } from './guards/for-logged-in.guard';
 
@@ -14,32 +13,28 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     redirectTo: 'register'
-  },
-  {
+  }, {
     path: 'login',
     children: [],
     component: LoginComponent
-}
+  },
   {
     path: 'register',
     component: RegistrationComponent,
     // canActivate: [ForLoggedInGuard]
-  },
-  {
+  }, {
     path: 'forget-password', // for testing forgetPasswordComponent, can be removed
     children: [],
     component: ForgetPasswordComponent
 
-    }, {
-      path: 'test-http', // for testing, can be removed
-      children: [],
-      component: TestHttpComponent
-    }, {
-      path: 'header', // for testing
-      component: HeaderViewComponent
-    }
-  },
-  {
+  }, {
+    path: 'test-http', // for testing, can be removed
+    children: [],
+    component: TestHttpComponent
+  }, {
+    path: 'header', // for testing
+    component: HeaderViewComponent
+  }, {
     path: 'restore-password', // for testing restorePasswordComponent, can be removed
     children: [],
     component: RestorePasswordComponent
@@ -54,4 +49,4 @@ const routes: Routes = [
   ]
 })
 
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpService } from './services/http.service';
-import { HttpClientModule } from '@angular/common/http';
-import { RegistrationComponent } from './components/registration/registration.component';
-import { LoginComponent } from './components/login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RestorePasswordComponent } from './components/restore-password/restore-password.component';
-import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
-import { TestHttpComponent } from './components/test-http/test-http.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HttpService} from './services/http.service';
+import {HttpClientModule} from '@angular/common/http';
+import {RegistrationComponent} from './components/registration/registration.component';
+import {LoginComponent} from './components/login/login.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RestorePasswordComponent} from './components/restore-password/restore-password.component';
+import {ForgetPasswordComponent} from './components/forget-password/forget-password.component';
+import {TestHttpComponent} from './components/test-http/test-http.component';
 import {OAuthComponent} from './components/login/o-auth/o-auth.component';
 
 import {
@@ -27,7 +27,7 @@ import {
   MdDialogModule,
   MdSlideToggleModule
 } from '@angular/material';
-import { HeaderViewComponent } from './components/header-view/header-view.component';
+import {HeaderViewComponent} from './components/header-view/header-view.component';
 import 'hammerjs';
 
 @NgModule({
@@ -37,7 +37,7 @@ import 'hammerjs';
     LoginComponent,
     ForgetPasswordComponent,
     TestHttpComponent,
-    HeaderViewComponent
+    HeaderViewComponent,
     RestorePasswordComponent,
     OAuthComponent
   ],
@@ -58,11 +58,10 @@ import 'hammerjs';
     MdDialogModule,
     MdSlideToggleModule,
     FormsModule,
-    HttpModule
+    HttpModule,
     ReactiveFormsModule
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
