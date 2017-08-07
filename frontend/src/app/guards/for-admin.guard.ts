8199d3ce1871ce67cce500f171763e6d4c361f37
Stas <chstas2@gmail.com>
2017-08-07 15:59:14 +0300

    changed to window.service

--- frontend/src/app/guards/for-admin.guard.ts
+++ frontend/src/app/guards/for-admin.guard.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { WindowObj } from './../services/window.service';
 
 @Injectable()
 export class ForAdminGuard implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private window: WindowObj) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
@@ -13,7 +14,8 @@ export class ForAdminGuard implements CanActivate {
   }
 
   isAdmin(): boolean {
-    if ((window as any)._injectedData.role === 'admin') { return true; }
+    if ((this.window.data._injectedData as any).role === 'admin') { return true; }
+    // without "as any" TS show error, when role doesn't excist in _injectedDate
 
     this.router.navigate(['/login']);   // can be change to main page or special-forbidden-page
     return false;
