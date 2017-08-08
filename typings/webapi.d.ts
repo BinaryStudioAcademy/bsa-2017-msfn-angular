2e485ee569b8ee5f20557f5911e0f6a4d7cf22d0
Dmitriy Beseda <besedadg@gmail.com>
2017-08-08 10:57:55 +0300

    webapi clean up

--- typings/webapi.d.ts
+++ typings/webapi.d.ts
@@ -1,9 +1,3 @@
-declare module global {
-    interface Window {
-        _injectedData: WebApi.IInjectedData;
-    }
-}
-
 declare module WebApi {
     export interface IWindow extends Window {
         _injectedData: IInjectedData;
