d9b59bc9e4aea42ce3bf28524dc28777a6cf7a75
Haosik <noobbaster@gmail.com>
2017-08-09 21:39:04 +0300

    small indentation fix

--- frontend/src/app/components/test-toastr/test-toastr.component.ts
+++ frontend/src/app/components/test-toastr/test-toastr.component.ts
@@ -2,37 +2,37 @@ import { Component, OnInit } from '@angular/core';
 import { ToastrService } from '../../services/toastr.service';
 
 @Component({
-  selector: 'app-test-toastr',
-  templateUrl: './test-toastr.component.html',
-  styleUrls: ['./test-toastr.component.scss']
+    selector: 'app-test-toastr',
+    templateUrl: './test-toastr.component.html',
+    styleUrls: ['./test-toastr.component.scss']
 })
 export class TestToastrComponent implements OnInit {
 
-  constructor(public toastrService: ToastrService) { }
+    constructor(public toastrService: ToastrService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  // Please check toastr.service.ts to see, that showMessage() expects to get 3 arguments:
-  // 1 - Type of message (it's color), 2 - Some text of message (may be null), 3 - Some heading of message (has defaults).
+    // Please check toastr.service.ts to see, that showMessage() expects to get 3 arguments:
+    // 1 - Type of message (it's color), 2 - Some text of message (may be null), 3 - Some heading of message (has defaults).
 
-  // Showing some Success (green) message to user
-  showSuccess() {
-    this.toastrService.showMessage('success', 'Some success message', 'Success Heading!');
-  }
+    // Showing some Success (green) message to user
+    showSuccess() {
+        this.toastrService.showMessage('success', 'Some success message', 'Success Heading!');
+    }
 
-  // e.g. User tried to log in, but was declined. It could look somthing like this:
-  userDeclined() {
-    this.toastrService.showMessage('error', 'Your login attempt was declined', 'Oh, sorry!');
-  }
+    // e.g. User tried to log in, but was declined. It could look somthing like this:
+    userDeclined() {
+        this.toastrService.showMessage('error', 'Your login attempt was declined', 'Oh, sorry!');
+    }
 
-  // Just notify user about something not so important
-  notifyUser() {
-    this.toastrService.showMessage('info', 'How are you?', 'What a nice day!');
-  }
+    // Just notify user about something not so important
+    notifyUser() {
+        this.toastrService.showMessage('info', 'How are you?', 'What a nice day!');
+    }
 
-  // Or maybe Warn user about something))
-  getSomeWarning() {
-    this.toastrService.showMessage('warning', 'Please, just wait a bit', 'Stop!');
-  }
+    // Or maybe Warn user about something))
+    getSomeWarning() {
+        this.toastrService.showMessage('warning', 'Please, just wait a bit', 'Stop!');
+    }
 }
--- frontend/src/app/services/http.service.ts
+++ frontend/src/app/services/http.service.ts
@@ -11,66 +11,66 @@ import { Router } from '@angular/router';
 @Injectable()
 export class HttpService {
 
-  private headers = new Headers({ 'Content-Type': 'application/json' });
-  public failMessage = 'Fail';
+    private headers = new Headers({ 'Content-Type': 'application/json' });
+    public failMessage = 'Fail';
 
-  constructor(private _http: Http, public toastrService: ToastrService, private _router: Router) { }
+    constructor(private _http: Http, public toastrService: ToastrService, private _router: Router) { }
 
-  private handleError = (error: any): Promise<any> => {
-    if (error && error.status === 401) {
-      this._router.navigate(['/login']);
+    private handleError = (error: any): Promise<any> => {
+        if (error && error.status === 401) {
+            this._router.navigate(['/login']);
+        }
+        this.toastrService.showMessage('error', error.message, this.failMessage);
+        return error;
     }
-    this.toastrService.showMessage('error', error.message, this.failMessage);
-    return error;
-  }
 
-  public sendRequest(options: IHttpReq): Promise<any> {
-    let url: string;
-    if (!options.url) {
-      return Promise.reject('Url required');
-    } else {
-      url = options.url;
-    }
-    const method = options.method || 'GET';
-    const body = options.body || {};
-    const successMessage: string = options.successMessage || 'Success';
-    this.failMessage = options.failMessage || this.failMessage;
-    const headers = options.headers || this.headers;
+    public sendRequest(options: IHttpReq): Promise<any> {
+        let url: string;
+        if (!options.url) {
+            return Promise.reject('Url required');
+        } else {
+            url = options.url;
+        }
+        const method = options.method || 'GET';
+        const body = options.body || {};
+        const successMessage: string = options.successMessage || 'Success';
+        this.failMessage = options.failMessage || this.failMessage;
+        const headers = options.headers || this.headers;
 
-    if (method === 'GET') {
-      return this._http.get(url)
-        .toPromise()
-        .then(response => {
-          this.toastrService.showMessage('success', null, successMessage);
-          return response.json();
-        })
-        .catch(this.handleError);
-    } else if (method === 'POST') {
-      return this._http
-        .post(url, body, { headers: headers })
-        .toPromise()
-        .then(response => {
-          this.toastrService.showMessage('success', null, successMessage);
-          return response.json();
-        })
-        .catch(this.handleError);
-    } else if (method === 'PUT') {
-      return this._http
-        .put(url, body, { headers: headers })
-        .toPromise()
-        .then(() => {
-          this.toastrService.showMessage('success', null, successMessage);
-          return body;
-        })
-        .catch(this.handleError);
-    } else if (method === 'DELETE') {
-      return this._http.delete(url, { headers: headers })
-        .toPromise()
-        .then(() => {
-          this.toastrService.showMessage('success', null, successMessage);
-          return null;
-        })
-        .catch(this.handleError);
+        if (method === 'GET') {
+            return this._http.get(url)
+                .toPromise()
+                .then(response => {
+                    this.toastrService.showMessage('success', null, successMessage);
+                    return response.json();
+                })
+                .catch(this.handleError);
+        } else if (method === 'POST') {
+            return this._http
+                .post(url, body, { headers: headers })
+                .toPromise()
+                .then(response => {
+                    this.toastrService.showMessage('success', null, successMessage);
+                    return response.json();
+                })
+                .catch(this.handleError);
+        } else if (method === 'PUT') {
+            return this._http
+                .put(url, body, { headers: headers })
+                .toPromise()
+                .then(() => {
+                    this.toastrService.showMessage('success', null, successMessage);
+                    return body;
+                })
+                .catch(this.handleError);
+        } else if (method === 'DELETE') {
+            return this._http.delete(url, { headers: headers })
+                .toPromise()
+                .then(() => {
+                    this.toastrService.showMessage('success', null, successMessage);
+                    return null;
+                })
+                .catch(this.handleError);
+        }
     }
-  }
 }
