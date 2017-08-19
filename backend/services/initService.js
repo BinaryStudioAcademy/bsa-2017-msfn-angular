a1030f2d7b7204c306de0c0423a73de54f70ace8
Alexandr Tovmach <alexandrtovmach@gmail.com>
2017-08-19 10:35:09 +0300

    addded initialize of measurments

--- backend/services/initService.js
+++ backend/services/initService.js
@@ -1,7 +1,8 @@
-var mongoose = require('mongoose'),
+const mongoose = require('mongoose'),
     userRepository = require('../repositories/userRepository'),
     passportOAuthStrategyInit = require('../middleware/passportOAuthMiddleware')(),
     passportStrategyInit = require('../middleware/passportStrategyMiddleware').strategy(),
+    measurementService = require('./measurementService'),
     adminData = {
         firstName: 'Arnold',
         lastName: 'Schwarzenegger',
@@ -12,16 +13,88 @@ var mongoose = require('mongoose'),
         isAdmin: true,
         position: 1,
         birthday: '2001-9-11',
-    }
+    },
+    measurments = [
+        {
+            "measureName": "weight",
+            "measureUnits": [
+                {"conversionFactor": "1", "unitName": "Kg", "unitType": "metric"},
+                {"conversionFactor": "2.20462", "unitName": "Lbs", "unitType": "imperial"}
+            ]
+        },
+        {
+            "measureName": "distances",
+            "measureUnits": [
+                {"conversionFactor": "1", "unitName": "Km", "unitType": "metric"},
+                {"conversionFactor": "1000", "unitName": "M", "unitType": "default"},
+                {"conversionFactor": "39370.1", "unitName": "inches", "unitType": "imperial"}
+            ]
+        },
+        {
+            "measureName": "temperature",
+            "measureUnits": [
+                {"conversionFactor": "1", "unitName": "°C", "unitType": "metric"},
+                {"conversionFactor": "32", "unitName": "°F", "unitType": "imperial"}
+            ]
+        },
+        {
+            "measureName": "timeFormat",
+            "measureUnits": [
+                {"conversionFactor": "1", "unitName": "24-hour clock" ,"unitType": "metric"},
+                {"conversionFactor": "0.5", "unitName": "12-hour clock", "unitType": "imperial"}
+            ]
+        },
+        {
+            "measureName": "dateFormat",
+            "measureUnits": [
+                {"conversionFactor": "1", "unitName": "European (day.month.year)" , "unitType": "metric"},
+                {"conversionFactor": "1", "unitName": "American (month/day/year)",  "unitType": "imperial"}
+            ]
+        },
+        {
+            "measureName": "startWeek",
+            "measureUnits": [
+                {"conversionFactor": "1", "unitName": "Monday", "unitType": "metric"},
+                {"conversionFactor": "1", "unitName": "Sunday",  "unitType": "imperial"}
+            ]
+        }
+    ]
+
+
+
 
 module.exports = function () {
+    // add measurments
+    measurementService.getAllMeasurements((err, data) => {
+        if (err) {
+            console.log('[InitService] - Error of initialize measurments:');
+            console.log(err);
+        } else if (data.length !== measurments.length) {
+            measurments.forEach(function(element) {
+                measurementService.createMeasurement(element, (err, data) => {
+                    if (err) {
+                        console.log('[InitService] - Error of adding Measurments:');
+                        console.log(err);
+                    } else {
+                        console.log('[InitService] - Measurments added');
+                    }
+                });
+            })
+        } else {
+            console.log('[InitService] - Measurments are available');
+        }
+    })
+
+    // add admin
     userRepository.getUserByEmail(adminData.email, (err, data) => {
         if (err) {
-            console.log('[InitService] - Error of initialize:' + err);
+            console.log('[InitService] - Error of initialize admin:');
+            console.log(err);
         } else if (data === null) {
             userRepository.add(adminData, (err, data) => {
                 if (err) {
-                    console.log('[InitService] - Error of adding:' + err);
+                    console.log('[InitService] - Error of adding admin:');
+                    console.log(err);
                 } else {
                     console.log('[InitService] - Admin added');
                 }
