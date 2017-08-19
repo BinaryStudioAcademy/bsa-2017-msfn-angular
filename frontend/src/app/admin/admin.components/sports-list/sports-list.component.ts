53f884d7a5c61e97238caec7844227d4f6b612f3
Roman Vey <roman.vey@gmail.com>
2017-08-20 00:25:23 +0300

    Added API to sport view

--- frontend/src/app/admin/admin.components/sports-list/sports-list.component.ts
+++ frontend/src/app/admin/admin.components/sports-list/sports-list.component.ts
@@ -1,100 +1,27 @@
+import { SportsListService } from './sports-list.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sports-list',
   templateUrl: './sports-list.component.html',
-  styleUrls: ['./sports-list.component.scss']
+  styleUrls: ['./sports-list.component.scss'],
+  providers: [SportsListService]
 })
 export class SportsListComponent implements OnInit {
 
- data = [{
-    name: 'Run',
-    code: 4
-  },
-  {
-    name: 'Bodybuilding',
-    code: 1
-  },
-  {
-    name: 'Jumping',
-    code: 1
-  },
-  {
-    name: 'Run',
-    code: 1
-  },
-  {
-    name: 'Bodybuilding',
-    code: 1
-  },
-  {
-    name: 'Jumping',
-    code: 1
-  },
-  {
-    name: 'Run',
-    code: 1
-  },
-  {
-    name: 'Bodybuilding',
-    code: 1
-  },
-  {
-    name: 'Jumping',
-    code: 1
-  },
-  {
-    name: 'Run',
-    code: 1
-  },
-  {
-    name: 'Bodybuilding',
-    code: 1
-  },
-  {
-    name: 'Jumping',
-    code: 1
-  },
-  {
-    name: 'Run',
-    code: 1
-  },
-  {
-    name: 'Bodybuilding',
-    code: 1
-  },
-  {
-    name: 'Jumping',
-    code: 1
-  },
-  {
-    name: 'Run',
-    code: 1
-  },
-  {
-    name: 'Bodybuilding',
-    code: 1
-  },
-  {
-    name: 'Jumping',
-    code: 1
-  },
-  {
-    name: 'Run',
-    code: 1
-  },
-  {
-    name: 'Bodybuilding',
-    code: 1
-  },
-  {
-    name: 'Jumping',
-    code: 1
-  }];
+  data: any[];
 
-  constructor() { }
+  constructor(private sportsListService: SportsListService) { }
 
   ngOnInit() {
+    this.sportsListService.getKindsOfSport((data) => {
+      if (data.length === 1 && !data[0].code) {
+        this.data = [];
+      } else {
+        this.data = data;
+      }
+
+    });
   }
 
 }
