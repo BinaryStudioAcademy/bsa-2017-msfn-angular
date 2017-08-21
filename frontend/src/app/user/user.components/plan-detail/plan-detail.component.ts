cfec275e628fb278d2badfe9b9bf33f55276da09
yumsterr <lina.vasileva93@gmail.com>
2017-08-21 09:52:33 +0300

    exercise table added

--- frontend/src/app/user/user.components/plan-detail/plan-detail.component.ts
+++ frontend/src/app/user/user.components/plan-detail/plan-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material';
 
 @Component({
   selector: 'app-plan-detail',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./plan-detail.component.scss']
 })
 export class PlanDetailComponent implements OnInit {
-  title = 'create';
+  title = 'Training plan create';
   trainingsCount = 0;
   exercsesShow = false;
+
   days = [
+    { 'key': 'md', 'value': 'Mon' },
+    { 'key': 'tu', 'value': 'Tue' },
+    { 'key': 'wd', 'value': 'Wed' },
+    { 'key': 'th', 'value': 'Thu' },
+    { 'key': 'fr', 'value': 'Fri' },
+    { 'key': 'sa', 'value': 'Sat' },
+    { 'key': 'su', 'value': 'Sun' }
+  ];
+
+  /*days = [
     { 'key': 'md', 'value': 'Monday' },
     { 'key': 'tu', 'value': 'Tuesday' },
     { 'key': 'wd', 'value': 'Wednesday' },
@@ -17,57 +29,226 @@ export class PlanDetailComponent implements OnInit {
     { 'key': 'fr', 'value': 'Friday' },
     { 'key': 'sa', 'value': 'Saturday' },
     { 'key': 'su', 'value': 'Sunday' }
-  ];
+  ]; */
 
   sportTypeValue: string;
   searchString: string = 'Coming soon...';
 
   types = [
-    {'value': 'run', 'viewValue': 'Run'},
-    {'value': 'fitness', 'viewValue': 'Fitness'},
-    {'value': 'loooong', 'viewValue': 'Tooooo long type name'},
+    { 'value': 'run', 'viewValue': 'Run' },
+    { 'value': 'fitness', 'viewValue': 'Fitness' },
+    { 'value': 'loooong', 'viewValue': 'Tooooo long type name' },
   ];
 
   exercisesList = [
     {
       id: 123,
-      name: 'ABS: MOUNTAIN CLIMBER WITH HANDS ON SWISS BALL',
+      order: 1,
+      name: 'Braced Squat',
+      type: 'fitness',
       description: 'It\'s one of the simplest yet most effective ways to tighten your tummy. In fact, you\'ll barely have to move a muscle.',
       how_to: 'Assume a pushup position with your arms completely straight, but place your hands on a Swiss ball instead of the floor. Your body should form a straight line from your head to your ankles. Tighten your core and hold it that way for the duration of the exercise [A]. Lift one foot off the floor and slowly raise your knee as close to your chest as you can without changing your lower-back posture. Then repeat with your other leg. Alternate back and forth for 30 seconds. If that\'s too hard, place your hands on the floor or a bench, instead of a Swiss ball.'
     },
     {
-      id: 233,
-      name: 'GLUTES: HIP RAISE',
-      description: ' It targets the muscles of your rear end, which can help make your belly flatter. The reason: When your glutes are weak—as they are in most women—the top of your pelvis tilts forward. This not only places stress on your lower back, but it causes your tummy to stick out—even if you don\'t have an ounce of fat. Your fix: the hip raise.',
-      how_to: 'Lie on your back on the floor with your knees bent and your feet flat on the floor [A]. Now brace your core, squeeze your glutes, and raise your hips so your body forms a straight line from your shoulders to your knees [B]. Pause for 3 to 5 seconds—squeezing your glutes tightly the entire time—then lower back to the start.'
+      id: 112,
+      name: 'Biceps Curls With SZ-bar',
+      type: 'fitness',
+      sets: [
+        {
+          weight: 30,
+          repeat: 5,
+        },
+        {
+          weight: 40,
+          repeat: 5,
+        },
+        {
+          weight: 45,
+          repeat: 5,
+        },
+
+      ],
+      description: 'Holding a weight on just one side of your body increases the demand placed on your core to keep your body stable. The result: Your hips and abs have to work harder, and you\'ll also improve your balance. And better yet, you\'ll burn tons of calories.',
+      how_to: '<p>Hold the SZ-bar shoulder-wide, the back is straight, the shoulders slightly back, the arms are streched. Bend the arms, bringing the weight up, with a fast movement. Without pausing, let down the bar with a slow and controlled movement.</p>\n<p>Don\'t allow your body to swing during the exercise, all work is done by the biceps, which are the only mucles that should move (pay attention to the elbows).</p>'
+    },
+    {
+      id: 123,
+      order: 1,
+      name: 'Braced Squat',
+      type: 'fitness',
+      description: 'It\'s one of the simplest yet most effective ways to tighten your tummy. In fact, you\'ll barely have to move a muscle.',
+      how_to: 'Assume a pushup position with your arms completely straight, but place your hands on a Swiss ball instead of the floor. Your body should form a straight line from your head to your ankles. Tighten your core and hold it that way for the duration of the exercise [A]. Lift one foot off the floor and slowly raise your knee as close to your chest as you can without changing your lower-back posture. Then repeat with your other leg. Alternate back and forth for 30 seconds. If that\'s too hard, place your hands on the floor or a bench, instead of a Swiss ball.'
     },
     {
       id: 112,
-      name: 'QUADRICEPS: OFFSET DUMBBELL LUNGE',
+      name: 'Biceps Curls With SZ-bar',
+      type: 'fitness',
+      sets: [
+        {
+          weight: 30,
+          repeat: 5,
+        },
+        {
+          weight: 40,
+          repeat: 5,
+        },
+        {
+          weight: 45,
+          repeat: 5,
+        },
+
+      ],
       description: 'Holding a weight on just one side of your body increases the demand placed on your core to keep your body stable. The result: Your hips and abs have to work harder, and you\'ll also improve your balance. And better yet, you\'ll burn tons of calories.',
-      how_to: 'Hold a dumbbell in your right hand next to your shoulder, with your arm bent [A]. Step forward with your right leg and lower your body until your right knee is bent at least 90 degrees and your left knee nearly touches the floor [B]. Push yourself back to the starting position. That\'s 1 rep. Do all your reps, then repeat with your left leg, while holding the weight in your left hand.'
+      how_to: '<p>Hold the SZ-bar shoulder-wide, the back is straight, the shoulders slightly back, the arms are streched. Bend the arms, bringing the weight up, with a fast movement. Without pausing, let down the bar with a slow and controlled movement.</p>\n<p>Don\'t allow your body to swing during the exercise, all work is done by the biceps, which are the only mucles that should move (pay attention to the elbows).</p>'
     },
     {
-      id: 443,
-      name: 'HAMSTRINGS: SINGLE-LEG DUMBBELL STRAIGHT-LEG DEADLIFT',
-      description: 'Besides targeting your hamstrings, this exercise works your glutes and core. It also helps eliminate muscle imbalances between your legs, reducing your risk of injury. And as a bonus, it can even improve the flexibility of your hamstrings.',
-      how_to: 'Grab a pair of dumbbells with an overhand grip, and hold them at arm\'s length in front your thighs. Stand with your feet shoulder-width apart and knees slightly bent. Now raise one leg off the floor [A]. Without changing the bend in your knee, bend at your hips (keep your lower back arched), and lower your torso until it\'s almost parallel to the floor [B]. Pause, then squeeze your glutes, thrust your hips forward, and raise your torso back to the start. Do all your reps, then repeat with your other leg.'
+      id: 123,
+      order: 1,
+      name: 'Braced Squat',
+      type: 'fitness',
+      description: 'It\'s one of the simplest yet most effective ways to tighten your tummy. In fact, you\'ll barely have to move a muscle.',
+      how_to: 'Assume a pushup position with your arms completely straight, but place your hands on a Swiss ball instead of the floor. Your body should form a straight line from your head to your ankles. Tighten your core and hold it that way for the duration of the exercise [A]. Lift one foot off the floor and slowly raise your knee as close to your chest as you can without changing your lower-back posture. Then repeat with your other leg. Alternate back and forth for 30 seconds. If that\'s too hard, place your hands on the floor or a bench, instead of a Swiss ball.'
     },
+    {
+      id: 112,
+      name: 'Biceps Curls With SZ-bar',
+      type: 'fitness',
+      sets: [
+        {
+          weight: 30,
+          repeat: 5,
+        },
+        {
+          weight: 40,
+          repeat: 5,
+        },
+        {
+          weight: 45,
+          repeat: 5,
+        },
+
+      ],
+      description: 'Holding a weight on just one side of your body increases the demand placed on your core to keep your body stable. The result: Your hips and abs have to work harder, and you\'ll also improve your balance. And better yet, you\'ll burn tons of calories.',
+      how_to: '<p>Hold the SZ-bar shoulder-wide, the back is straight, the shoulders slightly back, the arms are streched. Bend the arms, bringing the weight up, with a fast movement. Without pausing, let down the bar with a slow and controlled movement.</p>\n<p>Don\'t allow your body to swing during the exercise, all work is done by the biceps, which are the only mucles that should move (pay attention to the elbows).</p>'
+    },
+    {
+      id: 123,
+      order: 1,
+      name: 'Braced Squat',
+      type: 'fitness',
+      description: 'It\'s one of the simplest yet most effective ways to tighten your tummy. In fact, you\'ll barely have to move a muscle.',
+      how_to: 'Assume a pushup position with your arms completely straight, but place your hands on a Swiss ball instead of the floor. Your body should form a straight line from your head to your ankles. Tighten your core and hold it that way for the duration of the exercise [A]. Lift one foot off the floor and slowly raise your knee as close to your chest as you can without changing your lower-back posture. Then repeat with your other leg. Alternate back and forth for 30 seconds. If that\'s too hard, place your hands on the floor or a bench, instead of a Swiss ball.'
+    },
+    {
+      id: 112,
+      name: 'Biceps Curls With SZ-bar',
+      type: 'fitness',
+      sets: [
+        {
+          weight: 30,
+          repeat: 5,
+        },
+        {
+          weight: 40,
+          repeat: 5,
+        },
+        {
+          weight: 45,
+          repeat: 5,
+        },
+
+      ],
+      description: 'Holding a weight on just one side of your body increases the demand placed on your core to keep your body stable. The result: Your hips and abs have to work harder, and you\'ll also improve your balance. And better yet, you\'ll burn tons of calories.',
+      how_to: '<p>Hold the SZ-bar shoulder-wide, the back is straight, the shoulders slightly back, the arms are streched. Bend the arms, bringing the weight up, with a fast movement. Without pausing, let down the bar with a slow and controlled movement.</p>\n<p>Don\'t allow your body to swing during the exercise, all work is done by the biceps, which are the only mucles that should move (pay attention to the elbows).</p>'
+    },
+    {
+      id: 123,
+      order: 1,
+      name: 'Braced Squat',
+      type: 'fitness',
+      description: 'It\'s one of the simplest yet most effective ways to tighten your tummy. In fact, you\'ll barely have to move a muscle.',
+      how_to: 'Assume a pushup position with your arms completely straight, but place your hands on a Swiss ball instead of the floor. Your body should form a straight line from your head to your ankles. Tighten your core and hold it that way for the duration of the exercise [A]. Lift one foot off the floor and slowly raise your knee as close to your chest as you can without changing your lower-back posture. Then repeat with your other leg. Alternate back and forth for 30 seconds. If that\'s too hard, place your hands on the floor or a bench, instead of a Swiss ball.'
+    },
+    {
+      id: 112,
+      name: 'Biceps Curls With SZ-bar',
+      type: 'fitness',
+      sets: [
+        {
+          weight: 30,
+          repeat: 5,
+        },
+        {
+          weight: 40,
+          repeat: 5,
+        },
+        {
+          weight: 45,
+          repeat: 5,
+        },
+
+      ],
+      description: 'Holding a weight on just one side of your body increases the demand placed on your core to keep your body stable. The result: Your hips and abs have to work harder, and you\'ll also improve your balance. And better yet, you\'ll burn tons of calories.',
+      how_to: '<p>Hold the SZ-bar shoulder-wide, the back is straight, the shoulders slightly back, the arms are streched. Bend the arms, bringing the weight up, with a fast movement. Without pausing, let down the bar with a slow and controlled movement.</p>\n<p>Don\'t allow your body to swing during the exercise, all work is done by the biceps, which are the only mucles that should move (pay attention to the elbows).</p>'
+    },
+    // {
+    //   id: 233,
+    //   name: 'glutes: hip raise',
+    //   type: 'fitness',
+    //   description: ' It targets the muscles of your rear end, which can help make your belly flatter. The reason: When your glutes are weak—as they are in most women—the top of your pelvis tilts forward. This not only places stress on your lower back, but it causes your tummy to stick out—even if you don\'t have an ounce of fat. Your fix: the hip raise.',
+    //   how_to: 'Lie on your back on the floor with your knees bent and your feet flat on the floor [A]. Now brace your core, squeeze your glutes, and raise your hips so your body forms a straight line from your shoulders to your knees [B]. Pause for 3 to 5 seconds—squeezing your glutes tightly the entire time—then lower back to the start.'
+    // },
+    // {
+
+    // {
+    //   id: 443,
+    //   name: 'HAMSTRINGS: SINGLE-LEG DUMBBELL STRAIGHT-LEG DEADLIFT',
+    //   type: 'fitness',
+    //   description: 'Besides targeting your hamstrings, this exercise works your glutes and core. It also helps eliminate muscle imbalances between your legs, reducing your risk of injury. And as a bonus, it can even improve the flexibility of your hamstrings.',
+    //   how_to: 'Grab a pair of dumbbells with an overhand grip, and hold them at arm\'s length in front your thighs. Stand with your feet shoulder-width apart and knees slightly bent. Now raise one leg off the floor [A]. Without changing the bend in your knee, bend at your hips (keep your lower back arched), and lower your torso until it\'s almost parallel to the floor [B]. Pause, then squeeze your glutes, thrust your hips forward, and raise your torso back to the start. Do all your reps, then repeat with your other leg.'
+    // },
   ];
+  pageSize = 3;
+  length = 10;
+  page = 1;
+  pageEvent: PageEvent;
+  displayExercises: Object[];
 
   constructor() { }
 
   ngOnInit() {
+    this.displayExercises = this.exercisesList.slice(0,3);
   }
-  changeTrainingCount(operation){
-    if(operation == 'dec' && this.trainingsCount > 0){
-      this.trainingsCount -= 1;
-    } else if(operation == 'inc' && this.trainingsCount < 7){
-      this.trainingsCount += 1;
+  changeTrainingCount(newValue: string, operation = '') {
+    console.log(operation);
+    if (!newValue) {
+      switch (operation) {
+        case 'dec':
+          if (this.trainingsCount > 0) {
+            this.trainingsCount -= 1;
+          }
+          break;
+        case 'inc':
+          if (this.trainingsCount < 7) {
+            this.trainingsCount += 1;
+          }
+          break;
+      }
+    } else {
+      if (parseInt(newValue)) {
+        this.trainingsCount = parseInt(newValue);
+      }
     }
+
+    if (this.trainingsCount > 7) {
+      this.trainingsCount = 7;
+    } else if (this.trainingsCount < 0) {
+      this.trainingsCount = 0;
+    }
+    console.log(this.trainingsCount);
   }
 
-  getExercises(){
+  getExercises() {
     this.exercsesShow = true;
   }
 }
