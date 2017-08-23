5b552cad4ef7446419f985e9e97bfcb36c8775c4
CamilleSS <rott1zed@gmail.com>
2017-08-23 22:23:24 +0300

    Add goals to dashboard

--- frontend/src/app/user/user.components/dashboard/dashboard.components/db-goals/db-goals.component.ts
+++ frontend/src/app/user/user.components/dashboard/dashboard.components/db-goals/db-goals.component.ts
@@ -16,6 +16,21 @@ export class DbGoalsComponent implements OnInit {
         title: 'Goals'
     };
 
+    goals = [
+        {
+            startDate: '2017-08-23',
+            endDate: '2017-08-30',
+            value: 'Run 3km for 7 days in a row',
+            achieved: false
+        },
+        {
+            startDate: '2017-08-23',
+            endDate: '2017-09-30',
+            value: 'Increase the number of pushups in a set to 50',
+            achieved: false
+        }
+    ];
+
     ngOnInit() {
     }
 
