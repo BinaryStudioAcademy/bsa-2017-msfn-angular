f55d672f1b5856c0f5aab104e51a3a4fbb449bf2
Alexandr Tovmach <alexandrtovmach@gmail.com>
2017-08-24 12:45:32 +0300

    refactor code

--- frontend/src/app/user/user.components/active-training/active-training.component.ts
+++ frontend/src/app/user/user.components/active-training/active-training.component.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
+import { MdDialog, MdDialogRef } from '@angular/material';
+
+import { FinishDialogComponent } from './active-training.components/finish-dialog/finish-dialog.component';
 
 @Component({
     selector: 'app-active-training',
     templateUrl: 'active-training.component.html',
     styleUrls: ['active-training.component.scss']
 })
-export class ActiveTrainingComponent {}
+export class ActiveTrainingComponent {
+    finishTrain: boolean;
+    burnedCallories = 1445;
+    time: any = {
+        total: '00:00:00:000',
+        warming: '00:00:00:000'
+    };
+    dialogRef: MdDialogRef<any>;
+
+    constructor(
+        private dialog: MdDialog
+    ) {}
+
+    onFinish(timeData, callback) {
+        this.dialogRef = this.dialog.open(FinishDialogComponent, {
+            data: {
+                time: timeData,
+                calories: this.burnedCallories,
+            }
+        });
+
+        this.dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                this.finishTrain = true;
+            }
+            setTimeout(() => { this.finishTrain = false; }, 1000);
+        });
+    }
+
+}
--- frontend/src/app/user/user.components/active-training/active-training.components/secundomer/secundomer.component.ts
+++ frontend/src/app/user/user.components/active-training/active-training.components/secundomer/secundomer.component.ts
@@ -1,57 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { FinishDialogComponent } from '../finish-dialog/finish-dialog.component';
-import { MdDialog, MdDialogRef } from '@angular/material';
+import { Component, OnInit, OnChanges, EventEmitter, Input, Output } from '@angular/core';
+import { SecundomerService } from './secundomer.service';
 
 @Component({
     selector: 'app-secundomer',
     templateUrl: 'secundomer.component.html',
-    styleUrls: ['secundomer.component.scss']
+    styleUrls: ['secundomer.component.scss'],
+    providers: [
+        SecundomerService
+    ]
 })
 
 
 
-export class SecundomerComponent implements OnInit {
-    exercises = [
-        {
-            name: 'Pumping',
-            count: 5
-        },
-        {
-            name: 'Sit-ups',
-            count: 2
-        },
-        {
-            name: 'Push ups',
-            count: 15
-        },
-        {
-            name: 'Jumping rope',
-            count: 40
-        },
-    ];
-    burnedCallories = 1445;
+export class SecundomerComponent implements OnInit, OnChanges {
 
-
-
-    warmOuts = [];
-    runned: boolean;
-    warming: boolean;
-    warmingTime: number = 0;
-    intervalID: number;
-    intervalLapID: number;
-    secndomerNum: number = 0;
-    secndomerLapNum: number = 0;
-    secndomerPreviousLapNum: number = 0;
-    lapCacheTime: number;
-    lapPauseTime: number;
-    cacheTime: number;
-    pauseTime: number;
-    resStyle: string;
-    dialogRef: MdDialogRef<any>;
+    @Input() finishTrain: boolean;
+    @Output() onFinish = new EventEmitter();
 
     constructor(
-        private dialog: MdDialog
+        public secundomerService: SecundomerService
     ) { }
+
     beautifierTime(millisecnods: number): string {
         const formatter = new Intl.DateTimeFormat('ru', {
             hour: '2-digit',
@@ -60,75 +29,33 @@ export class SecundomerComponent implements OnInit {
         });
         return formatter.format(millisecnods - 3000 * 60 * 60) + ':' + String(millisecnods).slice(-3);
     }
+
     run(id: string): void {
-        this.runned = true;
-        if (this.warming) { this.warmingUp(); }
-        const start = this.cacheTime = (this.cacheTime) ? (Date.now() - (this.pauseTime - this.cacheTime)) : Date.now();
-        this.intervalID = setInterval(() => {
-            this.secndomerNum = (Date.now() - start);
-        }, 52);
+        this.secundomerService.run();
     }
     pause(): void {
-        this.pauseTime = Date.now();
-        this.lapPauseTime = (this.lapCacheTime) ? Date.now() : undefined;
-        this.runned = false;
-        clearInterval(this.intervalID);
-        clearInterval(this.intervalLapID);
+        this.secundomerService.pause();
+    }
+    warmingUp(): void {
+        this.secundomerService.warmingUp();
+    }
+    endWarmingUp(): void {
+        this.secundomerService.stopWarm();
     }
     finish(): void {
-        this.pause();
-        this.warming = false;
-        this.secndomerNum = 0;
-        this.pauseTime = 0;
-        this.cacheTime = 0;
-        this.lapCacheTime = 0;
-        this.lapPauseTime = 0;
-        this.secndomerLapNum = 0;
-        this.secndomerPreviousLapNum = 0;
-        this.warmOuts = [];
+        const data = {
+            total: this.beautifierTime(this.secundomerService.secndomerNum),
+            warming: this.beautifierTime(this.secundomerService.warmingTime)
+        };
+        this.onFinish.emit(data);
     }
 
-    warmingUp(): void {
-        this.warming = true;
-        const startLap = this.lapCacheTime = (this.lapCacheTime) ? (Date.now() - (this.lapPauseTime - this.lapCacheTime)) : Date.now();
-        this.intervalLapID = setInterval(() => {
-            this.secndomerLapNum = (Date.now() - startLap);
-        }, 52);
+    ngOnChanges(changes) {
+        if (changes.finishTrain.currentValue) {
+            this.secundomerService.stopTimers();
+        }
     }
 
-    endWarmingUp(): void {
-        this.warming = false;
-        clearInterval(this.intervalLapID);
-        this.lapCacheTime = 0;
-        this.lapPauseTime = 0;
-        this.warmOuts.push({
-            total: this.beautifierTime(this.secndomerNum),
-            lap: this.beautifierTime(this.secndomerLapNum),
-        });
-        this.warmingTime += this.secndomerLapNum;
-        this.secndomerPreviousLapNum = this.secndomerLapNum;
-    }
-
-    ngOnInit() {
-
-    }
-
-    openFinishDialog() {
-        this.dialogRef = this.dialog.open(FinishDialogComponent, {
-            data: {
-                time: {
-                    total: this.beautifierTime(this.secndomerNum),
-                    warming: this.beautifierTime(this.warmingTime)
-                },
-                calories: this.burnedCallories,
-                remainExercises: this.exercises,
-            }
-    });
-        this.dialogRef.afterClosed().subscribe(result => {
-            if (result) {
-                this.finish();
-            }
-        });
-    }
+    ngOnInit() {}
 
 }
