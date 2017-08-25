46651e7913ee74ccb8a1dc134518925d41a4e9c1
CamilleSS <rott1zed@gmail.com>
2017-08-25 10:20:05 +0300

    Start to implement burned calories

--- frontend/src/app/user/user.components/dashboard/dashboard.components/db-calories/db-calories.component.ts
+++ frontend/src/app/user/user.components/dashboard/dashboard.components/db-calories/db-calories.component.ts
@@ -16,6 +16,41 @@ export class DbCaloriesComponent implements OnInit {
         title: 'Burned Calories'
     };
 
+    burnedCalories = [
+        {
+            date: '2017-08-21',
+            amount: '300'
+        },
+        {
+            date: '2017-08-22',
+            amount: '320'
+        },
+        {
+            date: '2017-08-23',
+            amount: '280'
+        },
+        {
+            date: '2017-08-24',
+            amount: '350'
+        },
+        {
+            date: '2017-08-25',
+            amount: '420'
+        },
+        {
+            date: '2017-08-26',
+            amount: '390'
+        },
+        {
+            date: '2017-08-27',
+            amount: '450'
+        },
+        {
+            date: '2017-08-28',
+            amount: '310'
+        },
+    ];
+
     ngOnInit() {
     }
 
