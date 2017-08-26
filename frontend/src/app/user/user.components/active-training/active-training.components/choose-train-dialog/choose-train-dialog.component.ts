92f3b3ae7c92b23664a51d724bad00711fde2bca
Alexandr Tovmach <alexandrtovmach@gmail.com>
2017-08-26 15:06:16 +0300

    pseudo guards to training dialogs

--- frontend/src/app/user/user.components/active-training/active-training.components/choose-train-dialog/choose-train-dialog.component.ts
+++ frontend/src/app/user/user.components/active-training/active-training.components/choose-train-dialog/choose-train-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormControl, FormGroup, Validators, ValidatorFn } from '@angular/forms';
 import { MdDialogRef } from '@angular/material';
 import { MD_DIALOG_DATA } from '@angular/material';
@@ -9,14 +10,18 @@ import { MD_DIALOG_DATA } from '@angular/material';
     styleUrls: ['./choose-train-dialog.component.scss'],
 })
 export class ChooseTrainDialogComponent implements OnInit {
-
+    emptyList: boolean = false;
     matchedTrain = [];
     constructor(
-        @Inject(MD_DIALOG_DATA) public data: any
+        @Inject(MD_DIALOG_DATA) public data: any,
+        private router: Router
     ) { }
 
     ngOnInit() {
         const day = new Date().getDay();
+        if (!this.data.length) {
+            this.emptyList = true;
+        }
         this.data.forEach(element => {
             if (element.days && element.days.length) {
                 element.days.forEach(el => {
@@ -26,6 +31,10 @@ export class ChooseTrainDialogComponent implements OnInit {
                 });
             }
         });
+    }
+
+    createPlan() {
+        this.router.navigate(['/user/training-plan/add']);
     }
 }
 
