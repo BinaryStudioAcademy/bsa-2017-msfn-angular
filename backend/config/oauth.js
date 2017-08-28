7397a65a82b759c7a85000166726c2e44361ed84
Stas <chstas2@gmail.com>
2017-08-28 23:03:36 +0300

    update oauth settings

--- backend/config/oauth.js
+++ backend/config/oauth.js
@@ -2,15 +2,15 @@ module.exports = function () {
     const config = require('../config');
     return {
         googleOptions: {
-            clientID: '568258344186-shnqjs18j6jdudhq4jhnpal75sfdvpp5.apps.googleusercontent.com',
-            clientSecret: 'TwqsDp1Sf4wNcIaqLdhJWtPH',
+            clientID: '83981840514-r86m39o3uimhacsgcug75b08s48necae.apps.googleusercontent.com',
+            clientSecret: '2ut7kPXMA8U59NMr5RIqAv8A',
             callbackURL: config.host.hostAddress + '/auth/google/callback',
             passReqToCallback: true
         },
         facebookOptions: {
-            clientID: '1492797114115423',
-            clientSecret: '4853afb46acb6b1286d4ca0bdbbe49c2',
-            accessToken: '1492797114115423|JQrFtHMVEKGt2DLNjo_v7peh--g',
+            clientID: '1326784904113778',
+            clientSecret: 'c8ad6aad932eb303703c87e806b83c7c',
+            accessToken: '1326784904113778|92zGwu5MGjB4zf6xLCR0-OSfckU',
             callbackURL: config.host.hostAddress + '/auth/facebook/callback',
             profileFields: [
                 'id', 'displayName', 'photos', 'email'
@@ -18,9 +18,9 @@ module.exports = function () {
             passReqToCallback: true
         },
         twitterOptions: {
-            consumerKey: '73eCrw6PYset3wPCsTtreVEey',
-            consumerSecret: '7u8viziKxzeUlNV2BLUT0E2vptUmBYCXxjEv65k8T4Heak9Riq',
-            accessToken: '310721290-GfIIq6iwuE3J929z2l1pwciYDQspRTIJLlrH1WoW',
+            consumerKey: 'TibDpakOasEdOni0YBtV5jvfp',
+            consumerSecret: 'Fl97g8Y14XyJ7oJTXeb3fUTqn0kS6SLjE3TeUOz5P29xA3yK0k',
+            accessToken: '902256293670477826-KkskFTh3ck9DWtQQM8sQ6ixpORm8jxH',
             callbackURL: config.host.hostAddress + '/auth/twitter/callback',
             includeEmail: true,
             passReqToCallback: true
