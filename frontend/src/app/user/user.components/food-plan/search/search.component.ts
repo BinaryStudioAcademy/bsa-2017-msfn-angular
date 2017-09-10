403f428fac50f2a7f46434288d7821ea429cb6a3
Kate Veryha <katrusia.veryha@gmail.com>
2017-09-10 21:19:51 +0300

    search food by type

--- frontend/src/app/user/user.components/food-plan/search/search.component.ts
+++ frontend/src/app/user/user.components/food-plan/search/search.component.ts
@@ -1,13 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { IFood } from '../../../../models/food';
+import { IFoodType } from '../../../../models/food-type';
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+    foods: IFood[];
+    types: IFoodType[];
 
-  constructor() { }
+  constructor() {
+      this.foods = [
+          {
+              name: 'Banana',
+            foodType: 'Fruit',
+            kcal: 132,
+            protein: 34,
+            fat: 12,
+            carbons: 56,
+            vendor: 'FFF',
+          }, {
+            name: 'Appleapple',
+            foodType: 'Fruit',
+            kcal: 54,
+            protein: 12,
+            fat: 9,
+            carbons: 56,
+            vendor: 'Простоквашино',
+          } , {
+          name: 'Peach',
+          foodType: 'Fruit',
+          kcal: 124,
+          protein: 10,
+          fat: 0,
+          carbons: 87,
+          vendor: 'Basfffld',
+          }
+      ];
+      this.types = [ {name: 'Dairy products', isRemoved: false}, { name: 'Fruit', isRemoved: false}];
+  }
 
   ngOnInit() {
   }
